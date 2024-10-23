dbbbddfa2526e69a5d9c9a82861c730469c6bc85
akashbhalotia <akashbhalotia10@gmail.com>
2024-10-23 22:24:13 +0530

    Add tests for GET operation

--- transcriptDataExtractor/tests.py
+++ transcriptDataExtractor/tests.py
@@ -6,8 +6,42 @@ from rest_framework import status
 
 
 class TranscriptTestCase(TestCase):
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        # Create some test transcript objects
+        cls.transcript1 = Transcript.objects.create(
+            title="Transcript 1",
+            file=SimpleUploadedFile("file1.txt",
+                                    b"Content of file 1. Jack earns Rs. 50,000 per month. His expenses are Rs. 10,000 per month. He has a house and a car.",
+                                    content_type="text/plain")
+        )
+        cls.transcript2 = Transcript.objects.create(
+            title="Transcript 2",
+            file=SimpleUploadedFile("file2.txt", b"Content of file 2", content_type="text/plain")
+        )
+
+    def test_get_transcripts_list(self):
+        # Perform GET request to the transcripts list endpoint
+        response = self.client.get('/api/transcripts/', format="json")
+
+        # Verify the response status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # If the response is paginated, we need to access the 'results' key
+        if 'results' in response.data:
+            transcripts = response.data['results']
+        else:
+            transcripts = response.data
+
+        # Filter the transcripts based on the titles created
+        filtered_transcripts = [t for t in transcripts if t['title'] in ["Transcript 1", "Transcript 2"]]
+
+        # Check that the response contains the correct number of transcripts
+        self.assertEqual(len(filtered_transcripts), 2)
+
+        # Check that the titles of the transcripts match
+        self.assertEqual(filtered_transcripts[0]['title'], self.transcript1.title)
+        self.assertEqual(filtered_transcripts[1]['title'], self.transcript2.title)
 
     def test_upload_file_success(self):
         # Create a mock file
